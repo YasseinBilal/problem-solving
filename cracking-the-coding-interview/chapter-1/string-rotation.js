d310c67070b84ca812e45af985de9e8ba0fee441
yasseinbilal <yassein.bilal@gmail.com>
2019-12-10 13:32:21 +0100

    fix a typo

--- cracking-the-coding-interview/chapter-1/string-rotation.js
+++ cracking-the-coding-interview/chapter-1/string-rotation.js
@@ -11,7 +11,7 @@
 
 // Time Complexity: O(n)
 function stringRotation(string1, string2) {
-    if (string1.length !== string2.lenght) {
+    if (string1.length !== string2.length) {
             return false;
     }
     return (string2 + string2).includes(string1);
